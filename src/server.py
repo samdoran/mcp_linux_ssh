7f6f52e432718546ad1026df588354083aada003
Sam Doran <github@samdoran.com>
2025-10-01 14:40:42 -0400

    Improve ssh command build and add more parameters
    Signed-off-by: Sam Doran <github@samdoran.com>

--- src/server.py
+++ src/server.py
@@ -13,6 +13,25 @@ class MCPError(Exception):
         self.msg = msg
 
 
+def build_ssh_options() -> list[str]:
+    ssh_options = {
+        "ControlPersist": "yes",
+        "StrictHostKeyChecking": "no",
+        "UserKnownHostsFile": "/dev/null",
+    }
+
+    return [
+        arg
+        for key, value in ssh_options.items()
+        for arg in ("-o", f"{key}={value}")
+    ]  # fmt: skip
+
+
+def build_ssh_command(host: str, command: str) -> list[str]:
+    opts = build_ssh_options()
+    ssh_command = [shutil.which("ssh"), "-tt", *opts, host, command, "; sleep 0"]
+
+    return ssh_command
 @mcp.tool()
 def run_ssh(host: str, command: str) -> dict[str, t.Any]:
     """Run an SSH command on a remote host"""
